f3851de19fd8746f276a614e47fe332d04489018
thayorch <thatcharnon.10210@gmail.com>
2024-09-04 22:12:19 +0700

    commited

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
     <v-footer class="d-block flex-column mt-2" style="max-height:65px;" >
     <div class="text-center">
         © Copyright {{ new Date().getFullYear() }}. All Rights Reserved<br>
-        Modify by <strong><a target="_blank" style="text-decoration: none; color: black;" href="https://www.facebook.com/lnwice0">Thatcharnon Maidee</a></strong>
+        Modify by <strong><a target="_blank" style="text-decoration: none; color: black;" href="https://github.com/thayorch">
+          TY_.Martinez. </a></strong>
     </div>
   </v-footer>
   </v-app>
